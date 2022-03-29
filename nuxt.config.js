d38dffdb04d48e67e2b035084233b3b4ed89cb68
Diego Costa <diego.machado.d.costa@gmail.com>
2022-03-29 01:33:04 -0300

    :wrench: Add nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,33 +2,54 @@ export default {
   target: 'static',
 
   head: {
-    title: 'star-wars-poc',
+    title: 'Star Wars | Proof of concept',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet preconnect',
+        crossorigin: true,
+        href: 'https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap',
+      },
+    ],
   },
 
   css: [{ src: '~/assets/stylus/main.styl', lang: 'styl' }],
 
   plugins: [],
 
-  components: true,
+  components: {
+    dirs: [
+      '~/components/core',
+      '~/components/ui',
+      '~/components/page',
+    ],
+  },
 
-  buildModules: [
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: ['@nuxtjs/eslint-module'],
 
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa'],
 
-  axios: {
-    baseURL: '/',
+  axios: {},
+
+  router: {
+    routes: [
+      {
+        name: 'index',
+        path: '/',
+        component: 'pages/characters/index.vue',
+      },
+      {
+        name: 'user',
+        path: '/user',
+        component: 'pages/characters/_id.vue',
+      },
+    ],
   },
 
   pwa: {
@@ -38,4 +59,4 @@ export default {
   },
 
   build: {},
-}
+};
